52dd7bd16917bdd2594eddf6d52731d15b384a6c
nosch41 <noschdar.sindy@yahoo.com>
2019-11-27 14:09:31 +0100

    Added and commented synchronized

--- U4/src/main/java/U4/U4/ToolTipManager3.java
+++ U4/src/main/java/U4/U4/ToolTipManager3.java
@@ -12,6 +12,8 @@ public class ToolTipManager3  {
 	private ToolTipManager3() {
 		System.out.println("Neues Singleton erzeugt.");
 	}
+
+	//the 'synchronized' keyword solves the problem of two threads simultaneously initialising the instance. Instead they have to wait for their individual turn for the method.
 	public static synchronized ToolTipManager3 getInstance() {
 		if(instance==null) {
 			instance=new ToolTipManager3();
